39b6ead40c94a28e9acfff566cb99e8d184f794b
Álvaro <ainiguezdislaa@gmail.com>
2023-02-20 15:30:23 +0100

    ej8 terminado

--- src/aplicacion/Principal.java
+++ src/aplicacion/Principal.java
@@ -1,2 +1,10 @@
-package aplicacion;public class Principal {
+package aplicacion;
+import dominio.ej2_8;
+
+public class Principal {
+    public static void main(String[] args) {
+        int [] lista= {1,2,4,5};
+        System.out.println("La suma de los elementos pares la lista es "+ej2_8.sumapareslista(lista,lista.length-1));
+
+    }
 }
--- src/dominio/ej2_8.java
+++ src/dominio/ej2_8.java
@@ -1,4 +1,29 @@
 package dominio;
 
-public class ej2_7 {
+public class ej2_8 {
+
+
+
+    public static int sumapareslista(int[]lista,int i) {
+        if(i==0) {
+            if(lista[i]%2==0) {
+                return lista[i];
+            }else {
+                return 0;
+            }
+        }
+
+        if(i!=0) {
+            if(lista[i]%2!=0) {
+                return sumapareslista(lista,i-1);
+            }
+        }
+        return lista[i]+sumapareslista(lista,i-1);
+
+
+    }
+
+
+
 }
+
